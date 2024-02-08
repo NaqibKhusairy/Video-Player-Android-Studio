7b70aecadf55d1bafc4700db5552679af6c54e21
NaqibKhusairy <naaqibkhusairy@gmail.com>
2024-02-08 14:11:49 +0800

    Initial commit

---
+++ app/src/main/java/com/naqb/videoplayer/MainActivity.java
@@ -0,0 +1,32 @@
+package com.naqb.videoplayer;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.idVideoView);
+
+        // Load video from raw resources
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.sinarpelangi;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        mediaController.setMediaPlayer(videoView);
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
+    }
+}
